70c94a2a0d12bb93895e79c7fdfa71b6747b9493
hamusitapi <hamusita@163.com>
2019-04-07 22:49:04 +0800

    移动端适配完毕

--- footer.php
+++ footer.php
@@ -1,7 +1,7 @@
 <footer class="mdui-center mdui-p-a-1 mdui-shadow-8">
     <div class="mdui-row mdui-p-a-1">
-        <div>TimeYears.cn &copy; 2019</div><hr/>
-        <div>Power by <a href="http://typecho.org" target="_blank">Typecho</a></div>
+        <div>Theme-<a href="https://github.com/hamusitapi/Typecho-Theme-TimeYears" target="_blank">TimeYears</a> &copy; 2019</div><hr/>
+        <div>Power-<a href="http://typecho.org" target="_blank">Typecho</a></div>
     </div>
 </footer>
 </div>
