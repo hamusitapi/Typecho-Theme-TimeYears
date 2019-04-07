44c833e516678007a127365f4fd207fd1291cb37
hamusitapi <hamusita@163.com>
2019-04-07 09:43:14 +0800

    git commit

---
+++ index.php
@@ -0,0 +1,20 @@
+/**
+ * 小哈的第一个typecho主题
+ * 
+ * @package TimeYears Theme 
+ * @author hamusita
+ * @version 1.0.0
+ * @link http://timeyears.cn
+ */
+<?php while($this->next()): ?>
+    <div class="post">
+	<h2 class="entry_title"><a href="<?php $this->permalink() ?>"><?php $this->title() ?></a></h2>
+	<div class="entry_data">
+	    Published by <a href="<?php $this->author->permalink(); ?>"><?php $this->author(); ?></a> on <?php $this->date('F j, Y'); ?> in <?php $this->category(','); ?>.
+	    <?php $this->commentsNum('%d Comments'); ?>.
+	</div>
+	<div class="entry_text">
+	    <?php $this->content('Continue Reading...'); ?>
+	</div>
+    </div>
+<?php endwhile; ?>
