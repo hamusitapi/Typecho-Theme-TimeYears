505f7c1c602ed0c0acfc6c0be78b6a9cb247384c
hamusita <hamusita@163.com>
2019-05-01 01:53:04 +0800

    凌晨保命

--- comments.php
+++ comments.php
@@ -1,43 +1,64 @@
-<section id="content" class="mdui-center mdui-shadow-8">
-<h4><?php $this->commentsNum('暂无评论', '只有一个评论"' . $this->title . '"', '已有 %d 个评论'); ?></h4>
-<ol id="comment_list">
-    <?php $this->comments()->to($comments); ?>
-    <?php while($comments->next()): ?>
-        <div class="mdui-center mdui-shadow-2">
-        第<?php echo $comments->sequence(); ?>楼
-            <div class="comment_data">
-                    <strong><?php $comments->author(); ?></strong>
-                    <a><?php $comments->date('Y-m-d'); ?> <?php $comments->date('H:i'); ?></a>
-                </div>
-            <div class="comment_body"><?php $comments->content(); ?></div>
+<?php if (!defined('__TYPECHO_ROOT_DIR__')) exit; ?>
+<?php function threadedComments($comments, $options) {
+    $commentClass = '';
+    if ($comments->authorId) {
+        if($comments->authorId == $comments->ownerId){
+            $commentClass .= ' by-author';
+        }
+    }
+    $commentLevelClass = $comments->levels > 0 ? ' comment-child' : ' comment-parent';
+?>
+
+    <div class="comment-item<?php echo $commentClass ?>" id="<?php $comments->theId(); ?>">
+        <?php $comments -> gravatar('150', 'wavatar'); ?>
+        <div class="content">
+            <div class="comment-meta">
+                <span class="comment-author"><?php $comments->author(); ?></span>
+                <time class="comment-time"><?php $comments->date(); ?></time>
+                <span class="comment-reply"><?php $comments->reply(); ?></span>
+            </div>
+            <?php $comments->content(); ?>
         </div>
-    <?php endwhile; ?>
-</ol>
-</section>
-<!-- 判断设置是否允许对当前文章进行评论 -->
+<?php if ($comments->children) { ?>
+        <div class="comment-children">
+            <?php $comments->threadedComments($options); ?>
+        </div>
+<?php } ?>
+    </div>
+<?php } ?>
+
 <?php if($this->allow('comment')): ?>
-
-    <h4 id="response">Leave a Reply</h4>
-
-    <!-- 输入表单开始 -->
-    <form method="post" action="<?php $this->commentUrl() ?>" id="comment_form">
-
-        <!-- 如果当前用户已经登录 -->
-        <?php if($this->user->hasLogin()): ?>
-            <!-- 显示当前登录用户的用户名以及登出连接 -->
-            <p>Logged in as <a href="<?php $this->options->adminUrl(); ?>"><?php $this->user->screenName(); ?></a>.
-            <a href="<?php $this->options->index('Logout.do'); ?>" title="Logout">Logout &raquo;</a></p>
-
-        <!-- 若当前用户未登录 -->
-        <?php else: ?>
-            <!-- 要求输入名字、邮箱、网址 -->
-        <p><input type="text" name="author" class="text" size="35" value="<?php $this->remember('author'); ?>" /><label>Name (Required)</label></p>
-        <p><input type="text" name="mail" class="text" size="35" value="<?php $this->remember('mail'); ?>" /><label>E-mail (Required *will not be published)</label></p>
-        <p><input type="text" name="url" class="text" size="35" value="<?php $this->remember('url'); ?>" /><label>Website</label></p>
-        <?php endif; ?>
-
-        <!-- 输入要回复的内容 -->
-     <p><textarea rows="10" cols="50" name="text"><?php $this->remember('text'); ?></textarea></p>
-     <p><input type="submit" value="Submit Comment" class="submit" /></p>
-    </form>
+        <section class="board">
+            <h3>参与讨论</h3>
+            <?php $this->comments()->to($comments); ?>
+            <div id="<?php $this->respondId(); ?>">
+                <form class="comment-form" id="<?php $this->respondId(); ?>" action="<?php $this->commentUrl() ?>" id="comment-form" role="form">
+                    <div class="cancel-reply"><?php $comments->cancelReply(); ?></div>
+                    <figure class="comment-avatar">
+<?php if($this->user->hasLogin()): ?>
+                        <img src="<?php echo Typecho_Common::gravatarUrl($this->user->mail, 150, 'X', 'mm', true) ?>"/>
+<?php else: ?>
+                        <img src="<?php $this->options->themeUrl('static/img/avatar.jpg'); ?>"/>
 <?php endif; ?>
+                    </figure>
+                    <fieldset>
+                        <textarea rows="2" name="text" placeholder="说下感想吧！" required><?php $this->remember('text'); ?></textarea>
+<?php if(!$this->user->hasLogin()): ?>
+                        <input type="text" name="author" placeholder="昵称 *：" value="<?php $this->remember('author'); ?>" required="">
+                        <input type="email" name="mail" placeholder="电邮 *：" value="<?php $this->remember('mail'); ?>"<?php if ($this->options->commentsRequireMail): ?> required<?php endif; ?>>
+                        <input type="url" name="url" placeholder="http://" value="<?php $this->remember('url'); ?>"<?php if ($this->options->commentsRequireURL): ?> required<?php endif; ?>>
+<?php else: ?>
+                        <p class="logined"><a href="<?php $this->options->profileUrl(); ?>"><?php $this->user->screenName(); ?></a>，欢迎回来！不是你？<a href="<?php $this->options->logoutUrl(); ?>" title="登出">登出</a></p>
+<?php endif; ?>
+                        <button class="btn submit" type="submit"></button>
+                    </fieldset>
+                </form>
+            </div>
+<?php if ($comments->have()): ?>
+        <?php $comments->listComments(array('before' => '<div class="comment-list">', 'after' => '</div>')); ?>
+        <?php $comments->pageNav('', '', 3, '...', array('wrapTag' => 'section', 'itemTag' => 'span')); ?>
+<?php else: ?>
+    <p class="no-comment">还没有评论呢！</p>
+<?php endif; ?>
+<?php endif; ?>
+        </section>
