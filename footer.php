f758391269408685fc4289cacb398b83c59141ac
hamusita <49346236+hamusitapi@users.noreply.github.com>
2019-06-01 19:57:55 +0800

    more

--- footer.php
+++ footer.php
@@ -1,11 +1,11 @@
 </main>
-<footer class="mdui-typo mdui-center mdui-m-b-1">
-    <div class="mdui-p-a-1">Copyright &copy; 2019 <a href="//www.timeyears.cn" target="_blank" title="github">TimeYears</a></div>
-    <div class="mdui-p-a-1">Theme by <a href="https://github.com/hamusitapi/Typecho-Theme-TimeYears" target="_blank">TimeYears</a></div>
-    <div class="mdui-p-a-1">Power by <a href="//typecho.org" target="_blank">Typecho</a></div>
-    <div id="backtop" onclick="backtop()">↑</div>
+<footer class="mdui-typo mdui-center mdui-m-b-1 " style="text-align: center;">
+    &copy; 2019 <a href="<?php $this->options->siteUrl(); ?>" target="_blank" title="github"><?php $this->options->title() ?></a>. 
+    All Rights Reserved. 
+    Theme by <a href="https://github.com/hamusitapi/Typecho-Theme-TimeYears" target="_blank">TimeYears</a>. 
+    Power by <a href="//typecho.org" target="_blank">Typecho</a>. 
 </footer>
-
+<div id="backtop" onclick="backtop()">↑</div>
 <script src="<?php $this->options->themeUrl('used.js'); ?>"></script>
 </body>
 </html>
